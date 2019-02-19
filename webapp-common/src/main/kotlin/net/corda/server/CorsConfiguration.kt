6c63f2f1a8863561c87b4f86d983d9adc5439855
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-19 12:14:35 +0000

    webserver not run

---
+++ webapp-common/src/main/kotlin/net/corda/server/CorsConfiguration.kt
@@ -0,0 +1,24 @@
+package net.corda.server
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+open class CorsConfiguration {
+
+    @Bean
+    open fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "*"
+                                )
+            }
+        }
+    }
+}
--- webserver/src/main/kotlin/net/corda/server/controllers/SellerController.kt
+++ webserver/src/main/kotlin/net/corda/server/controllers/SellerController.kt
@@ -2,25 +2,21 @@ package net.corda.server.controllers
 
 import com.yunxi.flows.PayFlow1
 import com.yunxi.states.Transaction
-import net.corda.core.contracts.ContractState
 import net.corda.core.identity.CordaX500Name
-import net.corda.core.messaging.vaultQueryBy
 import net.corda.core.utilities.getOrThrow
-import net.corda.server.NodeRPCConnection
 import net.corda.server.models.TransactionInfo
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.time.LocalDateTime
-import java.time.ZoneId
 import net.corda.client.jackson.JacksonSupport
+import net.corda.server.NodeRPCConnection
 
 /**
  * A CorDapp-agnostic controller that exposes standard endpoints.
  */
 @RestController
 @RequestMapping("/") // The paths for GET and POST requests are relative to this base path.
-class StandardController(
+class SellerController(
         private val rpc: NodeRPCConnection) {
 
     companion object {
@@ -49,31 +45,4 @@ class StandardController(
         }
 
     }
-
-    @GetMapping(value = "/status", produces = arrayOf("text/plain"))
-    private fun status() = "200"
-
-    @GetMapping(value = "/servertime", produces = arrayOf("text/plain"))
-    private fun serverTime() = LocalDateTime.ofInstant(proxy.currentNodeTime(), ZoneId.of("UTC")).toString()
-
-    @GetMapping(value = "/addresses", produces = arrayOf("text/plain"))
-    private fun addresses() = proxy.nodeInfo().addresses.toString()
-
-    @GetMapping(value = "/identities", produces = arrayOf("text/plain"))
-    private fun identities() = proxy.nodeInfo().legalIdentities.toString()
-
-    @GetMapping(value = "/platformversion", produces = arrayOf("text/plain"))
-    private fun platformVersion() = proxy.nodeInfo().platformVersion.toString()
-
-    @GetMapping(value = "/peers", produces = arrayOf("text/plain"))
-    private fun peers() = proxy.networkMapSnapshot().flatMap { it.legalIdentities }.toString()
-
-    @GetMapping(value = "/notaries", produces = arrayOf("text/plain"))
-    private fun notaries() = proxy.notaryIdentities().toString()
-
-    @GetMapping(value = "/flows", produces = arrayOf("text/plain"))
-    private fun flows() = proxy.registeredFlows().toString()
-
-    @GetMapping(value = "/states", produces = arrayOf("text/plain"))
-    private fun states() = proxy.vaultQueryBy<ContractState>().states.toString()
 }
