dce86754e308fdc6a09c1e6b0cd81b822db61431
yunxi.zhang <yunxi.zhang@accenture.com>
2019-02-20 13:24:01 +0000

    add cordapp-buyer module

--- webapp-buyer/src/main/kotlin/net/corda/server/controllers/BuyerController.kt
+++ webapp-buyer/src/main/kotlin/net/corda/server/controllers/BuyerController.kt
@@ -25,24 +25,4 @@ class BuyerController(
 
     private val proxy = rpc.proxy
     private val mapper = JacksonSupport.createDefaultMapper(rpc.proxy)
-
-    @PostMapping("/pay")
-    private fun pay(@RequestBody info: TransactionInfo): ResponseEntity<String> {
-        try {
-            val tx = Transaction(
-                    sellerBalance = info.sellerBalance,
-                    buyerBalance = info.buyerBalance,
-                    payment = info.payment,
-                    seller = proxy.wellKnownPartyFromX500Name(CordaX500Name(info.seller, "London", "GB"))
-                            ?: throw IllegalArgumentException("Unknown party name."),
-                    buyer = proxy.wellKnownPartyFromX500Name(CordaX500Name(info.buyer, "London", "GB"))
-                            ?: throw IllegalArgumentException("Unknown party name.")
-            )
-            val result = proxy.startFlowDynamic(PayFlow1::class.java, tx).returnValue.getOrThrow()
-            return ResponseEntity.ok(mapper.writeValueAsString(result))
-        } catch (e: Exception) {
-            return ResponseEntity.badRequest().body("error:$e")
-        }
-
-    }
 }
